616290ccecd671105738ed8ea32bcf0c38907e76
Ubuntu <koepke@gmail.com>
2019-12-29 09:32:23 +0000

    debugging, nothing to keep

--- DerbyDay/settings.py
+++ DerbyDay/settings.py
@@ -90,7 +90,6 @@ WSGI_APPLICATION = 'DerbyDay.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
